53de2e973adb0b7fec9b4ddd43e695b26add3801
matas234 <160629386+matas234@users.noreply.github.com>
2024-06-24 01:32:34 +0100

    Deciphered tile encoding

---
+++ hand_xml_to_web.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+# formatting hand into web format from mahjong 1.0
+def webFormat(handArray):
+    dict = {0: 'm',
+        1: 'p',
+        2: 's',
+        3: 'z'         #note the ordering here: manzu, pinzu, souzu (same as paper)
+    }
+    split_indices=[9,18,27]
+    handArray =  np.split(handArray, split_indices) 
+    string = ''
+    k=0
+    for suit in handArray:
+        if sum(suit) == 0:
+            k+=1
+            continue
+        for num in range(len(suit)):
+            if suit[num] == 0:
+                continue
+            else:
+                string += str(num+1)*suit[num]
+
+        string += dict[k]
+        k+=1
+
+    return string
+
+def format_xmlHand(string):
+    out=np.zeros(34, dtype=int)
+    string_list = string.split(",")
+    array = np.array([int(i) for i in string_list])
+    for i in array:
+        out[i // 4] +=1
+    return webFormat(out)
+
+
+
+value="10,68,125,36,44,42,43,45,35,78,56,72,128"
+print(format_xmlHand(value))
