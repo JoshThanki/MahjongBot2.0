084adae370b25c9e9017a68070cada49921432b0
JoshThanki <joshthanki@gmail.com>
2024-06-26 18:42:35 +0100

    matrixfy

---
+++ matrixtest.py
@@ -0,0 +1,292 @@
+
+import json
+import random
+import numpy as np
+from numpy.typing import NDArray
+
+from xml.dom import minidom
+import xml.etree.ElementTree as ET
+
+import bz2
+import sqlite3
+from lxml import etree
+from tqdm import tqdm
+
+import sqlite3
+
+dbfile = '2020.db'
+
+con = sqlite3.connect(dbfile)
+
+cur = con.cursor()
+
+res = cur.execute("SELECT log_id, log_content FROM logs")
+
+logs = []
+
+for i in range(3):
+    logs.append(res.fetchone())
+
+con.close()
+
+out = {}
+
+
+
+tile_dic = {i: f"{i+1}m" if i <= 8 else f"{i-8}p" if i <= 17 else f"{i-17}s" for i in range(27)}
+honour_entries = {27 : "e", 28 : "s", 29 : "w", 30 : "n", 31 : "wd", 32 : "gd", 33 : "rd"}
+tile_dic.update(honour_entries)
+
+
+playerDict = {
+        0 : "e",
+        1 : "s",
+        2: "w",
+        3 : "n"
+    }
+
+# formatting hand into web format from mahjong 1.0
+def webFormat(handArray):
+    dict = {0: 'm',
+        1: 'p',
+        2: 's',
+        3: 'z'         #note the ordering here: manzu, pinzu, souzu (same as paper)
+    }
+    split_indices=[9,18,27]
+    handArray =  np.split(handArray, split_indices) 
+    string = ''
+    k=0
+    for suit in handArray:
+        if sum(suit) == 0:
+            k+=1
+            continue
+        for num in range(len(suit)):
+            if suit[num] == 0:
+                continue
+            else:
+                string += str(num+1)*suit[num]
+
+        string += dict[k]
+        k+=1
+
+    return string
+
+def format_xmlHand(string):
+    if string == "":
+        return ""
+
+    out=np.zeros(34, dtype=int)
+    string_list = string.split(",")
+    array = np.array([int(i) for i in string_list])
+    for i in array:
+        out[i // 4] +=1
+    return webFormat(out)
+
+
+class Matrix:
+    windDict = {
+        "e":0,
+        "s": 1,
+        "w": 2,
+        "n": 3,
+    }
+    
+    tile_dic = {i: f"{i+1}m" if i <= 8 else f"{i-8}p" if i <= 17 else f"{i-17}s" for i in range(27)}
+    honour_entries = {27 : "e", 28 : "s", 29 : "w", 30 : "n", 31 : "wd", 32 : "gd", 33 : "rd"}
+    tile_dic.update(honour_entries)
+
+    revtile_dic = {v: k for k, v in tile_dic.items()}
+            
+    def __init__(self):
+
+        self.gameState = np.zeros((11, 34))
+    
+    def returnMatrix(self):
+        return self.gameState
+    
+
+    def setRoundWind(self, wind):
+        windNum = self.windDict[wind]
+        self.gameState[0][0] = windNum
+
+    def getRoundWind(self):
+        reversedWindDict = {v: k for k, v in Game.windDict.items()}
+        return reversedWindDict[self.gameState[0][0]]
+    
+
+    def setDealer(self, dealer):
+        self.gameState[0][1] = dealer
+    
+    def getDealer(self):
+        return self.gameState[0][1]
+
+    def setHonbaSticks(self, amount):
+        self.gameState[0][3] = amount
+    
+    def getHonbaSticks(self):
+        return self.gameState[0][3]
+    
+    def setRiichiSticks(self, amount):
+        self.gameState[0][4] = amount
+    
+    def getRiichiSticks(self):
+        return self.gameState[0][4]
+    
+    def setWallTiles(self, amount):
+        self.gameState[0][5] = amount
+    
+    def decWallTiles(self):
+        self.gameState[0][5] -=1
+
+    def getWallTiles(self):
+        return self.gameState[0][5]
+    
+    def setPlayerScore(self, player, score):
+        if player in [0,1,2,3]:
+            self.gameState[0][6+player] = score
+        else:
+            print("Invalid Player")
+    
+    def getPlayerScore(self, player):
+        if player in [0,1,2,3]:
+            return self.gameState[0][6+player]
+        else:
+            print("Invalid Player")
+
+    def setPlayerRiichi(self, player):
+        if player in [0,1,2,3]:
+            self.gameState[0][10+player] = 1
+        else:
+            print("Invalid Player")
+    
+    def getPlayerRiichiStat(self, player):
+        if player in [0,1,2,3]:
+            return self.gameState[0][10+player]
+        else:
+            print("Invalid Player")
+    
+    def addDoraIndicator(self, doraIndicator: str):
+        self.gameState[1][self.strToIndex[doraIndicator]] += 1
+    
+    def getDoraIndicators(self):
+        return self.vectorToReadable(self.gameState[1])
+    
+    @staticmethod
+    def readableToVector(readableHand: list):
+        vectorHand = np.zeros(34)
+        for i in readableHand:
+            vectorHand[Game.strToIndex[i]] += 1
+        return vectorHand
+    
+    @staticmethod
+    def vectorToReadable(vectorHand: NDArray):
+        readableHand = []
+        for i in range(34):
+            for j in range(int(vectorHand[i])):
+                readableHand.append(Game.indexToStr[i])
+        return readableHand
+    
+    #game action functions:
+
+
+def matrixify(arr):
+    newArr = []
+
+    matrix = Matrix()
+
+
+
+
+    for item in arr:
+        if item[1]:
+
+            if item[0] == "INIT":
+                attr = item[1]
+
+                points = attr["ten"].split(",")
+
+                newArr.append("Initialise: Dealer " + attr["oya"] + " | p0 , score " + points[0] + ", hand " + format_xmlHand(attr["hai0"]) + " |  p1,  score  " + points[1] + ", hand " + format_xmlHand(attr["hai1"]) + " | p2,  score " + points[2] + ", hand " + format_xmlHand(attr["hai2"]) + " | p3 ,  score " + points[3] + ", hand " + format_xmlHand(attr["hai3"]))
+                newArr.append((item[0], item[1]))
+
+            elif item[0] == "N":
+                attr = item[1]
+                player = playerDict[int(attr["who"])]
+                meld = attr["m"]
+
+                newArr.append(player + " " + "CALLS" + " " + meld)
+            
+            else:
+                newArr.append((item[0], item[1]))
+        else:
+
+            if item[0][0] == "T":
+                newArr.append("p0 draw" + " " + tile_dic[(int(item[0][1:]) // 4)])
+
+            elif item[0][0] == "U":
+                newArr.append("p1 draw" + " " + tile_dic[(int(item[0][1:]) // 4)])
+
+            elif item[0][0] == "V":
+                newArr.append("p2 draw" + " " + tile_dic[(int(item[0][1:]) // 4)])
+                
+            elif item[0][0] == "W":
+                newArr.append("p3 draw" + " " + tile_dic[(int(item[0][1:]) // 4)])
+            
+
+            if item[0][0] == "D":
+                newArr.append("p0 discard" + " " + tile_dic[(int(item[0][1:]) // 4)])
+
+            elif item[0][0] == "E":
+                newArr.append("p1 discard" + " " + tile_dic[(int(item[0][1:]) // 4)])
+
+            elif item[0][0] == "F":
+                newArr.append("p2 discard" + " " + tile_dic[(int(item[0][1:]) // 4)])
+                
+            elif item[0][0] == "G":
+                newArr.append("p3 discard" + " " + tile_dic[(int(item[0][1:]) // 4)])
+
+    return newArr
+
+
+def covertLog(log):
+
+    game = log[0]
+
+    blob = log[1]
+
+    XML = etree.XML
+
+    decompress = bz2.decompress
+
+    content = decompress(blob)
+
+    xml = XML(content, etree.XMLParser(recover=True))
+
+    rough_string = ET.tostring(xml, encoding='unicode')
+
+    root = ET.fromstring(rough_string)
+
+    arr = []
+
+    for element in root:
+
+        header_name = element.tag
+        
+        attributes_dict = element.attrib
+
+        arr.append((header_name ,  attributes_dict))
+
+
+    arr = matrixify(arr)
+
+    return game, arr
+
+
+out = [covertLog(log) for log in logs]
+
+with open("out.txt", "w+") as file:
+    json.dump(out, file, indent = 2)
+
+
+
+
+
--- xmlFilter.py
+++ xmlFilter.py
@@ -31,45 +31,49 @@ con.close()
 
 out = {}
 
+# strToIndex = {
+#         "1_char": 0,
+#         "2_char": 1,
+#         "3_char": 2,
+#         "4_char": 3,
+#         "5_char": 4,
+#         "6_char": 5,
+#         "7_char": 6,
+#         "8_char": 7,
+#         "9_char": 8,
+#         "1_circ": 9,
+#         "2_circ": 10,
+#         "3_circ": 11,
+#         "4_circ": 12,
+#         "5_circ": 13,
+#         "6_circ": 14,
+#         "7_circ": 15,
+#         "8_circ": 16,
+#         "9_circ": 17,
+#         "1_bamb": 18,
+#         "2_bamb": 19,
+#         "3_bamb": 20,
+#         "4_bamb": 21,
+#         "5_bamb": 22,
+#         "6_bamb": 23,
+#         "7_bamb": 24,
+#         "8_bamb": 25,
+#         "9_bamb": 26,
+#         "e_wind": 27,
+#         "s_wind": 28,
+#         "w_wind": 29,
+#         "n_wind": 30,
+#         "w_drag": 31,
+#         "g_drag": 32,
+#         "r_drag": 33
+#     }
 
-strToIndex = {
-        "1m": 0,
-        "2m": 1,
-        "3m": 2,
-        "4m": 3,
-        "5m": 4,
-        "6m": 5,
-        "7m": 6,
-        "8m": 7,
-        "9m": 8,
-        "1p": 9,
-        "2p": 10,
-        "3p": 11,
-        "4p": 12,
-        "5p": 13,
-        "6p": 14,
-        "7p": 15,
-        "8p": 16,
-        "9p": 17,
-        "1s": 18,
-        "2s": 19,
-        "3s": 20,
-        "4s": 21,
-        "5s": 22,
-        "6s": 23,
-        "7s": 24,
-        "8s": 25,
-        "9s": 26,
-        "E": 27,
-        "S": 28,
-        "W": 29,
-        "N": 30,
-        "Wd": 31,
-        "Gd": 32,
-        "Rd": 33
-    }
+# indexToStr = {v: k for k, v in strToIndex.items()}
 
-indexToStr = {v: k for k, v in strToIndex.items()}
+tile_dic = {i: f"{i+1}_man" if i <= 8 else f"{i-8}_pin" if i <= 17 else f"{i-17}_sou" for i in range(27)}
+honour_entries = {27 : "East", 28 : "South", 29 : "West", 30 : "North", 31 : "White", 32 : "Green", 33 : "Red"}
+tile_dic.update(honour_entries)
+
 
 playerDict = {
         0 : "e",
@@ -140,29 +144,29 @@ def printNice(arr):
         else:
 
             if item[0][0] == "T":
-                newArr.append("E draw: " + indexToStr[(int(item[0][1:]) // 4)])
+                newArr.append("e draw" + " " + tile_dic[(int(item[0][1:]) // 4)])
 
             elif item[0][0] == "U":
-                newArr.append("S draw: " + indexToStr[(int(item[0][1:]) // 4)])
+                newArr.append("s draw" + " " + tile_dic[(int(item[0][1:]) // 4)])
 
             elif item[0][0] == "V":
-                newArr.append("W draw: " + indexToStr[(int(item[0][1:]) // 4)])
+                newArr.append("w draw" + " " + tile_dic[(int(item[0][1:]) // 4)])
                 
             elif item[0][0] == "W":
-                newArr.append("N draw: " + indexToStr[(int(item[0][1:]) // 4)])
+                newArr.append("n draw" + " " + tile_dic[(int(item[0][1:]) // 4)])
             
 
             if item[0][0] == "D":
-                newArr.append("E discard: " + indexToStr[(int(item[0][1:]) // 4)])
+                newArr.append("e discard" + " " + tile_dic[(int(item[0][1:]) // 4)])
 
             elif item[0][0] == "E":
-                newArr.append("S discard: " + indexToStr[(int(item[0][1:]) // 4)])
+                newArr.append("s discard" + " " + tile_dic[(int(item[0][1:]) // 4)])
 
             elif item[0][0] == "F":
-                newArr.append("W discard: " + indexToStr[(int(item[0][1:]) // 4)])
+                newArr.append("w discard" + " " + tile_dic[(int(item[0][1:]) // 4)])
                 
             elif item[0][0] == "G":
-                newArr.append("N discard: " + indexToStr[(int(item[0][1:]) // 4)])
+                newArr.append("n discard" + " " + tile_dic[(int(item[0][1:]) // 4)])
 
     return newArr
 
