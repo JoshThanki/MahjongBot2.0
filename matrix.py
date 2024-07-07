d51a8f38cbc27794886e406d23c1c7185018a58a
Skullshock986 <68910462+Skullshock986@users.noreply.github.com>
2024-07-07 22:33:24 +0100

    Add getPlayerScores to matrix

--- matrix.py
+++ matrix.py
@@ -173,6 +173,9 @@ class Matrix:
     def getWallTiles(self):
         return self.wallTiles
     
+    def getPlayerScores(self, scores):
+        return self.playerScores
+    
     def setPlayerScore(self, scores):
         self.playerScores = scores
     
