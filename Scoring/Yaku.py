d4081ff8619f263ed99c0334dd8b087b0c71e152
Skullshock986 <68910462+Skullshock986@users.noreply.github.com>
2024-07-18 22:39:01 +0100

    started working on yaku

--- Scoring/Yaku.py
+++ Scoring/Yaku.py
@@ -1,20 +1,53 @@
 import numpy as np
+from gameData import GameData
+import checkYaku 
 
 #gameData, player, typeWin, tile
 
-def riichi(gameData, player):
-    return gameData.getRiichi(player)
+yakus = {}
+
+# ONE HAN CLOSED ONLY
 
 def Menzenchin_tsumohou(gameData, player, typeWin):
-    return (typeWin==3) and (gameData.getClosed(player))
+    if (typeWin==3) and (gameData.getClosed(player)):
+        yakus[0] = 1
+
+def Riichi(gameData, player):
+    if gameData.getRiichi(player):
+        yakus[1] = 1
+
+#def Ippatsu(gameData, player):
+    # Will do
 
 
-def Pinfu(gameData, player):
-    if (gameData.getClosed(player)) and typeWin==1:
-        return Fu()==30   #placeholder function
-    
-    else:
-        return Fu()==20
+def Pinfu(gameData, player, typeWin):
+    if (gameData.getClosed(player)) and (checkYaku.getSeq() == 4):
+        if checkYaku.getLastSet()[0] != 1 or checkYaku.getLastSet()[1] != 9:
+            yakus[3] = typeWin
+        
+
+
+def Iipeikou(gameData, player):
+    if (gameData.getClosed(player)):
+        hand = checkYaku.checkYaku(gameData.privateHands[player])
+        
+        tuples = [tuple(trip) for trip in hand]
+
+        count_set = {}
+        duplicate_array = []
+
+        for t in tuples:
+            if t in count_set:
+                count_set.remove(t)
+                duplicate_array.append(t)  # Save the duplicate array
+            else:
+                count_set.add(t)
+
+        if len(count_set) > 0:
+            yakus[4] = len(count_set)
+        
+
+# ONE HAN
 
 
 
