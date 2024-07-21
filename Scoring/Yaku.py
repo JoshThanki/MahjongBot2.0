faa8a0eb9a59be64f9f5557cb29fe992f03ef0ab
Skullshock986 <68910462+Skullshock986@users.noreply.github.com>
2024-07-21 14:10:27 +0100

    More 2 han yakus

--- Scoring/Yaku.py
+++ Scoring/Yaku.py
@@ -77,5 +77,52 @@ def Yakuhai(gameData, player):
 
 # TWO HAN
 
+# def Double_riichi (will do)
+
+def Chintaiyao(gameData, player):
+    hand = checkYaku.checkYaku(gameData.privateHands[player])
+    target_numbers = {0, 8, 9, 17, 18, 26, 27, 28, 29, 30, 31, 32, 33}
+    check = True
+
+    for i in range (0, 5):
+        if hand[i][0] not in target_numbers and hand[i][len(hand[i]) - 1] not in target_numbers:
+            check = False
+            break
+    
+    if check == True:
+        yakus[12] = 1
+
+def Sanshoku_doujun(gameData, player):
+    seqs = checkYaku.getSequences(player)
+    if len(seqs) > 2:
+        # Will complete
+
+# def Ittsu (will do)
+    
+def Toitoi(gameData, player):
+    if len(checkYaku.getTriplets(player)) == 4:
+        yakus[15] = 1
+
+def Sanankou_or_doukou(gameData, player):
+    trips = checkYaku.getPrivSets(player)
+    if len(trips) >= 3:
+        for trip in trips:
+            # will complete
+
+def Sankantsu(gameData, player):
+    sets = checkYaku.getPrivSets(player)
+    if len(sets) >= 3:
+        yakus[18] = 1
+        for set in sets:
+            if len(set) != 4:
+                del yakus[18]
+                break
+
+# def chitoitsu (will do)
+
+
+                
+
+
 
 
